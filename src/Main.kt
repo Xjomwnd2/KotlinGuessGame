5dd65700c288d59993f5bd5721105d13a4f6b0c6
YourGitHubUsername <jmwaura@byupathway.edu>
2025-07-10 03:09:52 +0300

    COMMIT

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,32 @@
 // src/Main.kt
 
+fun checkGuess(guess: Int, target: Int): String {
+    return when {
+        guess < target -> "Too low!"
+        guess > target -> "Too high!"
+        else -> "Correct!"
+    }
+}
+
 fun main() {
     val numberToGuess = (1..100).random()
-    var guess: Int?
     println("Welcome to Guess the Number!")
     println("I am thinking of a number between 1 and 100.")
-
+    
+    var guess: Int?
     do {
         print("Enter your guess: ")
         guess = readLine()?.toIntOrNull()
-        if (guess == null) {
-            println("Please enter a valid number.")
-        } else if (guess < numberToGuess) {
-            println("Too low!")
-        } else if (guess > numberToGuess) {
-            println("Too high!")
-        } else {
-            println("Correct! You guessed the number!") 
+        
+        when {
+            guess == null -> println("Please enter a valid number.")
+            guess < 1 || guess > 100 -> println("Please enter a number between 1 and 100.")
+            else -> {
+                val result = checkGuess(guess, numberToGuess)
+                println(result)
+            }
         }
     } while (guess != numberToGuess)
+    
+    println("Thanks for playing!")
 }
